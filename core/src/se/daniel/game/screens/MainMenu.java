ae10b758e74df490ab081063737c6393f9d1bb0a
Daniel Gustafsson <danielg@narrativeteam.com>
2015-03-01 20:19:29 +0100

    added functionallity to buttons

--- core/src/se/daniel/game/screens/MainMenu.java
+++ core/src/se/daniel/game/screens/MainMenu.java
@@ -1,26 +1,55 @@
 package se.daniel.game.screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MainMenu implements Screen{
 	private Stage stage = new Stage();
 	private Table table = new Table();
 	
 	private Skin testSkin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+	private Label title = new Label("Achtung Die Kurve", testSkin);
 	private TextButton playButton = new TextButton("play", testSkin);
 	private TextButton optionsButton = new TextButton("options", testSkin);
 	private TextButton exitButton = new TextButton("exit", testSkin);
 	@Override
 	public void show() {
-		table.add(playButton).row();
-		table.add(optionsButton).row();
-		table.add(exitButton).row();
+		//add listeners
+		playButton.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				((Game) Gdx.app.getApplicationListener()).setScreen(new Splash());
+			}
+		});
+		
+		optionsButton.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				((Game) Gdx.app.getApplicationListener()).setScreen(new Splash());
+			}
+		});
+		
+		exitButton.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Gdx.app.exit();
+			}
+		});
+		
+		
+		table.add(title).padBottom(20).row();
+		table.add(playButton).size(150,60).padBottom(10).row();
+		table.add(optionsButton).size(150,60).padBottom(10).row();
+		table.add(exitButton).size(150,60).padBottom(10).row();
 		table.setFillParent(true);
 		stage.addActor(table);
 		
